fac5eff0dc7a9a8020e166c4d50679c35f1602c8
Chip Warden <lgw4@icloud.com>
2021-08-14 13:52:37 -0500

    Ignore function-cdf.bash if not on macOS.

--- dot_bashrc.d/function-cdf.bash
+++ dot_bashrc.d/function-cdf.bash
@@ -1,12 +1,10 @@
 # shellcheck shell=bash
 # cdf(): cd to the PATH of the front Finder window
-if [[ "${OSTYPE}" == darwin* ]]; then
-    cdf() {
-        target="$(osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)')"
-        if [[ "${target}" != "" ]]; then
-            cd "${target}" && pwd
-        else
-            echo "Error: no Finder window found." >&2
-        fi
-    }
-fi
+cdf() {
+    target="$(osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)')"
+    if [[ "${target}" != "" ]]; then
+        cd "${target}" && pwd
+    else
+        echo "Error: no Finder window found." >&2
+    fi
+}
