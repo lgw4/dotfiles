e32630cc62a19bf5a43e72cb6f6af68337b34a63
Chip Warden <lgw4@icloud.com>
2022-05-14 19:23:37 -0500

    Z Shell configuration refactoring.

--- private_dot_zshrc.d/private_paths/private_path-python.zsh
+++ private_dot_zshrc.d/private_paths/private_path-python.zsh
@@ -11,7 +11,6 @@ fi
 if command -v pyenv >/dev/null 2>&1 && [[ ":${PATH}:" != *":${HOME}/.pyenv/shims:"* ]]; then
     eval "$(pyenv init --path)"
     eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
     export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 fi
 
