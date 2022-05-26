f5802085424abcec3f55aac3e4b766bee2c56ff7
Chip Warden <lgw4@icloud.com>
2022-05-25 20:11:50 -0500

    Update get-info function for Z Shell.

--- private_dot_zshrc.d/private_functions/private_function-get-info.zsh
+++ private_dot_zshrc.d/private_functions/private_function-get-info.zsh
@@ -1,10 +1,6 @@
 #!/usr/bin/env zsh
-realpath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
-
 get-info() {
-    local FILE=$(realpath "${1}")
+    local FILE="{$1:A}"
     if [[ -r "${FILE}" ]]; then
         osascript <<EOF >/dev/null 2>&1
 set aFile to (POSIX file "file:///$FILE") as alias
