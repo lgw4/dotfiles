2920c2cbd25f24f9cd25097a73b84e9cf3182881
Chip Warden <lgw4@icloud.com>
2022-08-06 21:54:44 -0500

    Remove outdated Fish shell completion.

--- private_dot_config/private_fish/private_completions/private_go.fish
+++
@@ -1,7 +0,0 @@
-function __complete_go
-    set -lx COMP_LINE (commandline -cp)
-    test -z (commandline -ct)
-    and set COMP_LINE "$COMP_LINE "
-    /Users/lgw4/Developer/go/bin/gocomplete
-end
-complete -f -c go -a "(__complete_go)"
