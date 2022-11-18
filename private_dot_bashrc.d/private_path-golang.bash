c4da7af13f6df2b0ef1f631c8f2a09f7c9f7d156
Chip Warden <lgw4@icloud.com>
2022-11-17 22:30:45 -0600

    Update Golang configuration.

--- private_dot_bashrc.d/private_path-golang.bash
+++ private_dot_bashrc.d/private_path-golang.bash
@@ -9,6 +9,7 @@ fi
 
 if command -v goenv >/dev/null 2>&1; then
     eval "$(goenv init -)"
+    path_prepend "$GOENV_ROOT"/.shims
 fi
 
 if [[ -d "$HOME"/Developer/go ]]; then
@@ -18,6 +19,9 @@ elif [[ -d "$HOME"/devel/go ]]; then
 fi
 
 if [[ -n "$GOPATH" ]]; then
-    path_append "$GOPATH"/bin 
+    path_append "$GOPATH"/bin
 fi
 
+if [[ -n "$GOENV_ROOT" ]] && [[ -n "$GOPATH" ]]; then
+    export GOENV_GOPATH_PREFIX="$GOPATH"
+fi
