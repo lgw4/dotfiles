a58eb181ff24f834cee249c8285f1cb5fda1769f
Chip Warden <lgw4@icloud.com>
2022-12-17 19:51:43 -0600

    Reorganize Z Shell completion settings.

--- private_dot_zshrc.d/private_settings/private_completion.zsh
+++ private_dot_zshrc.d/private_settings/private_completion.zsh
@@ -6,3 +6,9 @@ setopt AUTO_PARAM_SLASH
 setopt COMPLETE_IN_WORD
 setopt HASH_LIST_ALL
 setopt NO_MENU_COMPLETE
+
+autoload -Uz compinit
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+compinit
