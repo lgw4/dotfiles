380901ced3a61a26e3aefe23be52a50824704b20
Chip Warden <lgw4@icloud.com>
2022-12-18 22:07:36 -0600

    Reformat.

--- private_dot_bashrc.d/private_configuration/private_history.bash
+++ private_dot_bashrc.d/private_configuration/private_history.bash
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 # Configure Bash history
 shopt -s histappend  # Append to the history file, don't overwrite it
-shopt -s cmdhist  # Save multi-line commands as one command
+shopt -s cmdhist     # Save multi-line commands as one command
 HISTSIZE=8192
 HISTFILESIZE=8192
 HISTCONTROL=erasedups:ignoreboth
@@ -10,13 +10,13 @@ HISTTIMEFORMAT='%F %T '
 
 # Taken from https://unix.stackexchange.com/a/48116
 _bash_history_sync () {
-    builtin history -a         #1
-    HISTFILESIZE=$HISTSIZE     #2
-    builtin history -c         #3
-    builtin history -r         #4
+    builtin history -a
+    HISTFILESIZE=$HISTSIZE
+    builtin history -c
+    builtin history -r
 }
 
-history() {                  #5
+history() {
     _bash_history_sync
     builtin history "$@"
 }
