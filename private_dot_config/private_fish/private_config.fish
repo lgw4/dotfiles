c2b40c04a85c922d3ecf98b957b9c0737810b5b5
Chip Warden <lgw4@icloud.com>
2023-04-15 00:01:14 -0500

    Update shell colorschemes.

--- private_dot_config/private_fish/private_config.fish
+++ private_dot_config/private_fish/private_config.fish
@@ -5,7 +5,7 @@ if type -q pipx
 end
 
 if type -q vf
-    set -x VIRTUALFISH_HOME $HOME/.local/share/virtualenvs
+    set -x VIRTUALFISH_HOME "$HOME"/.local/share/virtualenvs
 end
 
 if type -q starship
@@ -16,6 +16,15 @@ if type -q direnv
     direnv hook fish | source
 end
 
-if test -e "$HOME/.iterm2_shell_integration.fish"
-    source $HOME/.iterm2_shell_integration.fish
+# Base16 Shell
+if test -d "$HOME"/.config/base16-shell
+    if status --is-interactive
+        set BASE16_SHELL "$HOME"/.config/base16-shell/
+        source "$BASE16_SHELL"/profile_helper.fish
+    end
+    base16-tomorrow
+end
+
+if test -e "$HOME/".iterm2_shell_integration.fish
+    source "$HOME"/.iterm2_shell_integration.fish
 end
