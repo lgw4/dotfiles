945c818e71c22bca084259a95a9f21777036369f
Chip Warden <lgw4@users.noreply.github.com>
2025-07-01 20:29:46 -0500

    Update goenv configuration.

--- private_dot_config/private_fish/private_conf.d/private_path-golang.fish
+++ private_dot_config/private_fish/private_conf.d/private_path-golang.fish
@@ -1,33 +1,11 @@
-if test -d "$HOME/.goenv"
-    set -x GOENV_ROOT $HOME/.goenv
-end
-
-if test -n "$GOENV_ROOT" && test -d "$GOENV_ROOT/bin"
-    fish_add_path -mpP $GOENV_ROOT/bin
-    function goenv-update -d "Update goenv installation"
-        set current_directory (pwd)
-        printf "Updating goenv…\n"
-        cd $GOENV_ROOT
-        git fetch
-        git pull
-        cd plugins
-        for directory in (command ls)
-            printf "Updating $directory…\n"
-            cd $directory
-            git fetch
-            git pull
-            cd ..
-        end
-        cd $current_directory
-        set -u current_directory
-    end
-end
-
 if type -q goenv && status --is-interactive
     goenv init - fish | source
-    fish_add_path -mpP $GOENV_ROOT/shims
 end
 
+if test -d "$HOME/.goenv"
+    set -x GOENV_ROOT $HOME/.goenv
+    fish_add_path -mpP $GOENV_ROOT/shims
+end
 
 if test -d "$HOME/Developer/go"
     set -x GOPATH $HOME/Developer/go
