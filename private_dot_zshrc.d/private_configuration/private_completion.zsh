10323aa8a570839def3e961576ec13298b156d84
Chip Warden <lgw4@users.noreply.github.com>
2026-06-30 19:43:47 -0500

    Bring Z Shell config into feature parity with Fish Shell.

---
+++ private_dot_zshrc.d/private_configuration/private_completion.zsh
@@ -0,0 +1,4 @@
+zstyle ':completion:*' menu select
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format '%B%d%b'
+zstyle ':completion:*' group-name ''
---
+++ private_dot_zshrc.d/private_configuration/private_greeting.zsh
@@ -0,0 +1,4 @@
+if [[ -o login ]] && command -v fortune >/dev/null 2>&1; then
+    echo
+    fortune
+fi
