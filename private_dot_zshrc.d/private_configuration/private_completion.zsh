69ba1efcabf4aad21d41f2fec767adf0e3668272
Chip Warden <lgw4@users.noreply.github.com>
2026-06-30 21:10:30 -0500

    Fix Z Shell completion.

--- private_dot_zshrc.d/private_configuration/private_completion.zsh
+++ private_dot_zshrc.d/private_configuration/private_completion.zsh
@@ -2,11 +2,16 @@
 zmodload zsh/complist
 
 # Raise past the default of 100 so large lists (e.g. `brew` has 113
-# subcommands) go straight into the scrollable menu instead of stopping
-# at "do you wish to see all N possibilities?" first.
+# subcommands) go straight to listing instead of stopping at
+# "do you wish to see all N possibilities?" first.
 LISTMAX=100000
 
 zstyle ':completion:*' menu select
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*:descriptions' format '%B%d%b'
 zstyle ':completion:*' group-name ''
+
+# Page long completion lists a screenful at a time instead of dumping
+# them past the terminal -- without this, LISTMAX above just prints
+# everything in one unbroken burst.
+zstyle ':completion:*' list-prompt '%SAt %p: TAB for more, or the character to insert%s'
