93b95abde91e7bbb689e47d4e69981af6566052e
Chip Warden <lgw4@users.noreply.github.com>
2026-06-30 20:57:43 -0500

    Fix Z Shell completion.

--- private_dot_zshrc.d/private_configuration/private_completion.zsh
+++ private_dot_zshrc.d/private_configuration/private_completion.zsh
@@ -1,3 +1,11 @@
+# zsh/complist implements menu-select; it is not autoloaded by compinit.
+zmodload zsh/complist
+
+# Raise past the default of 100 so large lists (e.g. `brew` has 113
+# subcommands) go straight into the scrollable menu instead of stopping
+# at "do you wish to see all N possibilities?" first.
+LISTMAX=100000
+
 zstyle ':completion:*' menu select
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*:descriptions' format '%B%d%b'
